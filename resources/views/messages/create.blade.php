7074da34ca450830225b6bc8cd94484289c4682f
777 <ec2-user@ip-172-31-34-229.us-east-2.compute.internal>
2018-05-17 05:37:28 +0000

    bootstrap design

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -4,14 +4,22 @@
 
 <h1>メッセージ新規作成ページ</h1>
 
-    {!! Form::model($message, ['route' => 'messages.store']) !!}
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($message, ['route' => 'messages.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        </div>
+    </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
+    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
